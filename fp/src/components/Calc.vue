cc38b16a9f06b53e4225299f7a7bbd1a3b234c75
StasBeep <stas-stas.stanislav@mail.ru>
2021-07-02 15:35:16 +0300

    добавление нового сомпонента для калькулятора с подключением к основному приложению

---
+++ fp/src/components/Calc.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <button>Click</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Calc",
+    data:() => ({
+
+    })
+}
+</script>
+
+<style>
+
+</style>
