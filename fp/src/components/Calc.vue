3b42aac031c795f5e9517a5cf9b06c9c4394db8a
StasBeep <stas-stas.stanislav@mail.ru>
2021-07-02 21:00:40 +0300

    реализация калькулятора внутри кнопок

--- fp/src/components/Calc.vue
+++ fp/src/components/Calc.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
-        <input v-on:focus="sum = 1 + 1"/>
-        <button v-on:click="sum = 3">Click</button>
+        <div>
+            <input type="number" placeholder="op1" v-model.number="operand1"> 
+            <input type="number" placeholder="op2" v-model.number="operand2">
+            = {{ sum }}
+        </div>
+        <div>
+            <button @click="sum = operand1 + operand2">+</button>
+            <button @click="sum = operand1 - operand2">-</button>
+            <button v-on:click="sum = operand1 * operand2">*</button>
+            <button>Event</button>
+        </div>
         <!--<div v-on:mouseover=""></div>-->
         result : {{ sum }}
     </div>
@@ -11,6 +20,8 @@
 export default {
     name: "Calc",
     data:() => ({
+        operand1: 0,
+        operand2: 0,
         sum: 0
     })
 }
