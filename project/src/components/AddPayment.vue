3c9db59900d0bcf8c1326d2a05be5018b925e91b
StasBeep <stas-stas.stanislav@mail.ru>
2021-07-11 23:18:25 +0300

    эмит - прокидывание данных наверх (this.())

--- project/src/components/AddPayment.vue
+++ project/src/components/AddPayment.vue
@@ -21,7 +21,26 @@ export default {
 
     methods: {
         onClick() {
+            const { category, value } = this
+            const data = {
+                date: this.date || this.getCurrentDate,
+                category,
+                value
+            }
 
+            // Вызов события, название события и аргументы
+
+            this.$emit('addNewPayment', data);
+        }
+    },
+
+    computed: {
+        getCurrentDate() {
+            const today = new Date();
+            const d = today.getDate();
+            const m = today.getMonth() + 1;
+            const y = today.getFullYear();
+            return `${d}.${m}.${y}`;
         }
     }
 }
