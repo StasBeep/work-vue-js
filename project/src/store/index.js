9c961a8c246ad2e6b1c3c19252d1898432352e77
StasBeep <stas-stas.stanislav@mail.ru>
2021-07-13 22:16:15 +0300

    Дефолтное заполнение храинилища (определённые методы) ` 	// Начальная точка отсчёта (состояние) 	state: {
    	 },
    
    	// Изменение данных
    	mutations: {
    
    	},
    
    	// Запускают mutations
    	// Запрос -> получение -> вызов действий mutations
            actions: {
    
    	},
    
    	// "Выдача" данных
    	getters: {
    
    	}
    `

--- project/src/store/index.js
+++ project/src/store/index.js
@@ -4,5 +4,24 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    
+    // Начальная точка отсчёта (состояние)
+    state: {
+        
+    },
+
+    // Изменение данных
+    mutations: {
+
+    },
+
+    // Запускают mutations
+    // Запрос -> получение -> вызов действий mutations
+    actions: {
+
+    },
+
+    // "Выдача" данных
+    getters: {
+
+    }
 })
