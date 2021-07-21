de2399b8fe53dc6d53b1ce03e0fb5e348994683c
StasBeep <stas-stas.stanislav@mail.ru>
2021-07-21 16:02:32 +0300

    заполнение router и подключение компонентов к roter

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -1,8 +1,39 @@
 import Vue from 'vue';
 import Router from 'vue-router'
 
+import Dashboard from '../views/Dashboard.vue'
+import About from '../views/About.vue'
+import NotFound from '../views/NotFound.vue'
+
 Vue.use(Router)
 
 export default new Router({
-    
+    // Убирает #
+    mode: 'history',
+    routes: [
+        { 
+            // Путь
+            path: '/dashboard',
+            // Компонент реализации
+            component: Dashboard,
+            // Имя роута (может понадобится в дальнейшем)
+            name: 'dashboard'
+        },
+        { 
+            // Путь
+            path: '/about',
+            // Компонент реализации
+            component: About,
+            // Имя роута (может понадобится в дальнейшем)
+            name: 'about'
+        },
+        { 
+            // Путь
+            path: '/notfound',
+            // Компонент реализации
+            component: NotFound,
+            // Имя роута (может понадобится в дальнейшем)
+            name: 'NotFound'
+        }
+    ]
 })
