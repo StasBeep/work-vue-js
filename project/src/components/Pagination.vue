8e9676a27ab2508287be702beccd1f7401acdfb5
StasBeep <stas-stas.stanislav@mail.ru>
2021-07-26 17:24:03 +0300

    добавление разметки пагинации

--- project/src/components/Pagination.vue
+++ project/src/components/Pagination.vue
@@ -1,10 +1,19 @@
 <template>
-
+    <div :class="[$style.wrp]">
+        <div @click="onClick(cur - 1)">-</div>
+        <div
+            v-for="i in amount" :key="i"
+            :class="{
+                [$style.active]: cur === i 
+            }"
+            @click="onClick(i)"
+        >{{ i }}</div>
+        <div @click="onClick(cur + 1)">+</div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: "Pagination",
     
 }
 </script>
