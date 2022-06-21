755cfac56884cddc0a65c3ebb1491adbe78e0b9f
StasBeep <stas-stas.stanislav@mail.ru>
2022-06-21 21:06:01 +0300

    первая лекция (повторение)

---
+++ fp/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Calc />
+    <HelloWorld msg="Welcome to Your Vue.js App" :myArr= "arr"/>
+    Text : {{ message }} длина {{ message.length }}
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Calc
+  },
+
+  data: () => ({
+    message: "Ваша реклама",
+    arr: [1,2,3,4,5]
+  })
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ fp/src/components/Calc.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div>
+            <input type="number" placeholder="op1" v-model.number="operand1">
+            <input type="number" placeholder="op2" v-model.number="operand2">
+            = {{ sum }}
+        </div>
+        <div>
+            <button @click="sum = operand1 + operand2">+</button>
+            <button @click="sum = operand1 - operand2">-</button>
+            <button @click="sum = operand1 * operand2">*</button>
+            <button @click="div">/</button>
+        </div>
+        <!--<div v-on:mouseover=""></div>-->
+        {{ sum }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CalcItem',
+
+    data: () => ({
+        operand1: 0,
+        operand2: 0,
+        sum: 0
+    }),
+
+    methods: {
+        div() {
+            this.sum = this.operand1 / this.operand2
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
