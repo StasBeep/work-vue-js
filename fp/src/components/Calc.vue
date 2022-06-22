d0506328bf3f86ce9e32ffee83f1e3c641e31fd2
StasBeep <stas-stas.stanislav@mail.ru>
2022-06-22 16:46:58 +0300

    вторая лекция (повторение)

--- fp/src/components/Calc.vue
+++ fp/src/components/Calc.vue
@@ -5,14 +5,24 @@
             <input type="number" placeholder="op2" v-model.number="operand2">
             = {{ sum }}
         </div>
-        <div>
-            <button @click="sum = operand1 + operand2">+</button>
-            <button @click="sum = operand1 - operand2">-</button>
-            <button @click="sum = operand1 * operand2">*</button>
-            <button @click="div">/</button>
+        <div class="collection">
+            <button v-for="(item, idx) in collection" :key="idx">
+                {{ item }}
+            </button>
         </div>
-        <!--<div v-on:mouseover=""></div>-->
-        {{ sum }}
+                <div class="buttons">
+            <button v-for="btn in buttons"
+                :key="btn"
+                @click="calculate(btn)">
+                    {{ btn }}
+            </button>
+        </div>
+        <div class="error" v-show="error"> Ошибка: {{ error }}</div>
+        <div class="strange-message">
+            <template v-if="sum < 0">Отрицательное число</template>
+            <template v-else-if="sum < 100"> Число меньше 100</template>
+            <template v-else>Число больше 100</template>
+        </div>
     </div>
 </template>
 
@@ -23,17 +33,66 @@ export default {
     data: () => ({
         operand1: 0,
         operand2: 0,
-        sum: 0
+        sum: 0,
+        buttons: ["+","-","*","/","^"],
+        collection: [0,1,2,3,4,5,6,7,8,9],
+        error: ""
     }),
 
     methods: {
+        calculate(operation = "+") {
+            this.error = ""
+            switch (operation) {
+                case "+":
+                    this.add();
+                    break;
+                case "-":
+                    this.substract();
+                    break;
+                case "*":
+                    this.multiply();
+                    break;
+                case "/":
+                    this.div();
+                    break;
+                case "^":
+                    this.degree();
+                    break;
+            }
+        },
+
+        add() {
+            this.sum = this.operand1 + this.operand2
+        },
+
+        substract() {
+            this.sum = this.operand1 - this.operand2
+        },
+
         div() {
-            this.sum = this.operand1 / this.operand2
+            const { operand1, operand2 } = this
+            if(operand2 === 0){
+                this.error = "На 0 делить нельзя!!"
+            } else {
+                this.sum = operand1 / operand2
+            }
+        },
+
+        multiply() {
+            this.sum = this.operand1 * this.operand2
+        },
+
+        degree() {
+            this.sum = Math.pow(this.operand1, this.operand2);
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
+
+.error {
+    color: red;
+}
 
 </style>
