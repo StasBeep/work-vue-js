0e9249dd06ffc41b2368884eedbd13b5c80ac67f
StasBeep <stas-stas.stanislav@mail.ru>
2023-04-29 01:28:56 +0300

    добавление файлов vue v3 есть ошибка, но работает с ней

---
+++ hello-vue/src/components/Btns.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button>button 1</button>
+    <button @click="changeSmt">button 2</button>
+    {{ smt }}
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'BtnsElement',
+
+    data() {
+      return {
+        smt: 0
+      }
+    },
+
+    methods: {
+      changeSmt() {
+        this.smt++;
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
